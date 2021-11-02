5b52dfe327d48a56f46232c235789cf1072e914e
Renato Coutinho <renato.coutinho@gmail.com>
2021-11-02 16:44:26 -0300

    corrigindo sort_files pra processar apenas arquivos passados na linha de comando

--- dados/sort_files.sh
+++ dados/sort_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for i in limpo_dados*.csv; do
-    (head -n 1 "${i}" && tail -n +2 "${i}" | sort -k 1 -t"," --parallel=4 -u ) > sorted_"${i}"
+
+# loop sobre arquivos da linha de comando
+for i "$@"; do
+    (head -n 1 "$i" && tail -n +2 "$i" | sort -k 1 -t"," --parallel=4 -u ) > "sorted_${i}.csv"
     echo "${i}"
     rm "${i}"
 done
