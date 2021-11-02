3dcab0988fc2090ffc1622dc3d591cad5cd848a7
Renato Coutinho <renato.coutinho@gmail.com>
2021-11-02 20:02:39 -0300

    split_file toma mais cuidado com arquivo tmp

--- dados/split_file.sh
+++ dados/split_file.sh
@@ -4,13 +4,16 @@ f=$1
 n=$2
 suffix=".csv"
 foo=${f%"$suffix"}
-echo "$f"
-tail -n +2 "$f" > tmp_file
-split tmp_file split_"$foo"_ --number=l/"$n" -d --additional-suffix=".csv"
+echo "splitting $f"
+
+# safely create temp file
+tmp_file=`mktemp -p .`
+tail -n +2 "$f" > $tmp_file
+split $tmp_file split_"$foo"_ --number=l/"$n" -d --additional-suffix=".csv"
 for file in split_"$foo"*.csv
 do
-    head -n 1 "$f" > tmp_file
-    cat "$file" >> tmp_file
-    mv -f tmp_file "$file"
+    head -n 1 "$f" > $tmp_file
+    cat "$file" >> $tmp_file
+    mv -f $tmp_file "$file"
 done
 
