55f3dc8908a8a328d3063e2fffa7eac763c361c7
Renato Coutinho <renato.coutinho@gmail.com>
2021-11-02 19:14:50 -0300

    corrigindo mais um bug no sorter

--- dados/sort_files.sh
+++ dados/sort_files.sh
@@ -2,7 +2,7 @@
 
 # loop sobre arquivos da linha de comando
 for i in "$@"; do
-    (head -n 1 "$i" && tail -n +2 "$i" | sort -k 1 -t"," --parallel=4 -u ) > "sorted_${i}.csv" &&
+    (head -n 1 "$i" && tail -n +2 "$i" | sort -k 1 -t"," --parallel=4 -u ) > "sorted_${i}" &&
         rm "${i}" &&
         echo "sorted ${i}"
 done
